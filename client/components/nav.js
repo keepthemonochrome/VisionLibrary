085ab9afa5da96c8666bb8c0c8c21af5258502b3
Louis Bergeron <louispvb@gmail.com>
2016-12-10 20:07:18 -0800

    Change color of input text to white

--- client/components/nav.js
+++ client/components/nav.js
@@ -1,17 +1,8 @@
 import React from 'react';
-import Dropzone from 'react-dropzone';
 import ReactDOM from 'react-dom';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import AutoComplete from 'material-ui/AutoComplete';
-import AppBar from 'material-ui/AppBar';
 import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
-import IconMenu from 'material-ui/IconMenu';
-import IconButton from 'material-ui/IconButton';
 import FontIcon from 'material-ui/FontIcon';
-import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more';
-import MenuItem from 'material-ui/MenuItem';
-import DropDownMenu from 'material-ui/DropDownMenu';
-import RaisedButton from 'material-ui/RaisedButton';
 import FlatButton from 'material-ui/FlatButton';
 import Upload from './upload.js';
 import Styles from './Styles'
@@ -40,6 +31,7 @@ class Nav extends React.Component {
             underlineStyle={Styles.searchUnderline}
             onNewRequest={searchStr => console.log(searchStr)}
             fullWidth={true}
+            inputStyle={{color: 'white'}}
             />
           <ToolbarGroup>
             <Upload />
